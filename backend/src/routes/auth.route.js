6599bf667cbc7e9ee1635b1a68f4d66f337206d5
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-28 01:16:48 +0530

    protectedRoute middleware engineered

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const route = express.Router();
 
@@ -7,4 +8,5 @@ route.post("/signup", signup);
 route.post("/login", login);
 route.post("/logout", logout);
 
+route.put("/update-profile", protectRoute, updateProfile)
 export default route;
