cc77fb627ca07564b48f1bd8b2bf2284ef2d6b00
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-28 15:41:52 +0530

    signup and navbar made responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import { LogOut, MessageCircleCode, MessageCircleHeartIcon, MessageSquare, Settings, Settings2, SettingsIcon, User } from "lucide-react";
+import { LogOut, MessageCircleCode,  SettingsIcon, User } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore"
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
-  
+
   return (
     <header
       className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80"
