1d687542d06f82f51f63bea2cfa59440314d2765
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-28 09:57:56 +0530

    message routes and cntrollers setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT, () => {
   connectDB();
