5eb15f8a631caa455445ec817317053a27a627b0
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-29 22:43:56 +0530

    Production build 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import cors from "cors"
+import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({
   limit: '100mb'
@@ -24,6 +26,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist/")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 server.listen(PORT, () => {
   connectDB();
