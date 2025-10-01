f9dac615626c94b46ec170e51364469aa23d58c1
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-10-01 23:26:23 +0530

    refactor: cleanup unused code

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose"
-
-export const connectDB = async() => {
+import mongoose from 'mongoose';
+const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB connnected at ${conn.connection.host}`)
-  } catch (error) {
-    console.log(`Error Connecting to DB: ${error.message}`)
-    process.exit(1)
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('DB connection error:', err);
+    process.exit(1);
   }
-}
+};
+export default connectDB;
